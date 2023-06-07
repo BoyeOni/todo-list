99639e39f0c737e19f7e59104a6324be6427c18e
Aboyede Oni-Orisan <Aboyede.o@gmail.com>
2023-06-07 14:20:40 -0500

    added text placeholder and add item button

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Container from "../components/container/Container";
 import Header from "../components/header/Header";
+import Body from "../components/body/Body";
+import AddTodo from "../components/add_todo/AddTodo";
 
 export default function Home() {
   return (
     <Container>
       <Header />
+      <AddTodo />
     </Container>
   );
 }
