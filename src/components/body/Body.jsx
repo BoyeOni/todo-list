8fd16ee215b77cbc223464d5e8be21d1f8113f9c
Aboyede Oni-Orisan <Aboyede.o@gmail.com>
2023-06-07 14:55:47 -0500

    added list and delete button

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -5,7 +5,17 @@ export default function Body() {
   return (
     <div className="listTodo">
       <div className="listTodo__row">
-        <div className="listTodo__item">Buy New Laptop</div>
+        <div className="listTodo__item">Do Coding Assignment</div>
+        <div className="listTodo__delete">Del</div>
+      </div>
+
+      <div className="listTodo__row">
+        <div className="listTodo__item">Work Out</div>
+        <div className="listTodo__delete">Del</div>
+      </div>
+
+      <div className="listTodo__row">
+        <div className="listTodo__item">Go For A Walk</div>
         <div className="listTodo__delete">Del</div>
       </div>
     </div>
